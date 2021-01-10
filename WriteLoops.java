78a8a8f2192ba9b7af18d521fd0603d1dd0e417d
'nortonlyr' <'nortonlyr@gmail.com'>
2021-01-10 00:14:26 -0500

    WriteLoops updated

--- WriteLoops.java
+++ WriteLoops.java
@@ -30,10 +30,12 @@ public class WriteLoops {
 
     public int oneToTen() {
         int w = 0;
+        for (int i=0; i<10; i++) {
 
         // Write a FOR loop that counts from 1 to 10.
         // calling
         w = w + 1;
+        }
         // each time through the loop
         
         return w;
@@ -41,10 +43,12 @@ public class WriteLoops {
 
     public int startAtTwentyOne() {
         int w = 0;
+        for (int i = 20; i < 32; i++) {
 
         // Write a FOR loop that makes 10 iterations, start at 21.
         // calling
         w = w + 1;
+        }
         // each time through the loop
         
         return w;
@@ -52,10 +56,11 @@ public class WriteLoops {
 
     public int countDown() {
         int w = 0;
-
+        for (int i = 100; i > 0; i--) { 
         // Write a FOR loop that counts down from 100 to 0.
         // calling
         w = w + 1;
+        }
         // each time through the loop
         
         return w;
@@ -63,20 +68,23 @@ public class WriteLoops {
 
     public int byTwoTo32() {
         int w = 0;
-
+        for (int i = 0; i < 32; i+=2) {
         // Write a FOR loop from 0 to 32 by 2s.
         // calling
         w = w + 1;
+        }
         // each time through the loop
         return w;
     }
 
     public int countDownFrom5000() {
         int w = 0;
+        for (int i = 0; i < 5001; i += 11) {
 
         // Write a FOR loop from 1 to less than 5001 by 11s.
         // calling
         w = w + 1;
+        }
         // each time through the loop
         
         return w;
@@ -84,13 +92,16 @@ public class WriteLoops {
 
     public int nestedFors() {
         int w = 0;
-
+        for (int i = 0; i < 20; i++) {
+        	w = w + 1;
+        	for (int j = 0; j < 4; j++) {
         // Write a nested FOR loop(s), where one counts from
         // 0 to less than 20 and the inner one counts from 0 to 4
                 // calling
                 w = w + 1;
+        	}
+        }
                 // each time through the inner loop
-
         return w;
     }
 
